4a0280b96e9527708c2965746ffd8a8d15932353
Pantawt <pantawat_b@cmu.ac.th>
2022-12-20 16:41:32 +0700

    3_s

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -2,9 +2,20 @@
 using namespace std;
 
 char before(char x){
-	//Write your function definition here
+	char y = toupper(x);
+if (x==y and x != '0')
+	{
+		if (x>'A')
+		{
+			x= x-1;
+		}else{
+			x='Z';
+		}
+	}else {
+		x='0';
+	}
+	return x ;
 }
-
 int main(){
 	//Test Case
 	cout << before('A') << "\n";
